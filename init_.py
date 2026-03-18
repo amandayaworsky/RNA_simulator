98fd58f999694ca45734c31481ff385b333e020d
amandayaworsky <amandayaworsky@gmail.com>
2026-03-18 18:52:43 -0400

    Create init_.py

---
+++ init_.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+rna-simulator: Generate synthetic RNA sequences.
+
+A Python package for creating synthetic RNA sequences with configurable
+characteristics for bioinformatics research.
+"""
+
+__version__ = "1.0.0"
+__author__ = "AmandaYaworsky
